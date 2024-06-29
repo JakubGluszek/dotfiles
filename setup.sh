531a6f79b5cab8cbc077b6f43b816dbca2e16b91
Jacob <jacobgluszek03@gmail.com>
2024-06-29 16:04:17 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Array of files/folders to symlink
-files=(".config/regolith2/Xresources" ".tmux.conf" ".zshrc")
+files=(".config/regolith3/Xresources" ".tmux.conf" ".zshrc")
 
 for file in "${files[@]}"; do
-  ln -sf "$HOME/dotfiles/$file" "$HOME/$file"
+  ln -sf "$HOME/.dotfiles/$file" "$HOME/$file"
 done
 
 echo "Dotfiles have been symlinked!"
